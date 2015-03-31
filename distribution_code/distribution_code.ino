333e2cb1e7f32d7bd2e2337f3507b69c409efb08
ldion <ldion78@gmail.com>
2015-03-31 10:24:58 -0400

    Added function headers to distribution code

--- distribution_code/distribution_code.ino
+++ distribution_code/distribution_code.ino
@@ -20,6 +20,12 @@ const int NUM_ENEMIES = 16;
 // a global variable that represents the LED screen
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+// the following functions are for printing messages
+void print_level(int level);
+void print_lives(int lives);
+void game_over();
+void player_wins();
+
 class Color {
   public:
     int red;
@@ -53,15 +59,59 @@ const Color AQUA(0, 7, 7);
 
 class Invader {
   public:
+    // Constructors
     Invader() {
       x = 0;
       y = 0;
       strength = 0;
     }
+    // sets values for private date members x and y
+    Invader(int x_arg, int y_arg) {
+    }
+    // sets values for private data members
+    Invader(int x_arg, int y_arg, int strength_arg) {
+    }
+    // sets values for private data members
+    void initialize(int x_arg, int y_arg, int strength_arg) {
+    }
+    
+    // getters
+    int get_x() const {
+    }
+    int get_y() const {
+    }
+    int get_strength() const {
+    }
+
+    // Moves the Invader down the screen by one row
+    // Modifies: y
+    void move() {
+    }
+    
+    // draws the Invader if its strength is greater than 0
+    // calls: draw_with_rgb
+    void draw() {
+    }
+    
+    // draws black where the Invader used to be
+    // calls: draw_with_rgb
+    void erase() {
+    }    
+    
+    // Invader is hit by a Cannonball.
+    // Modifies: strength
+    // calls: draw, erase
+    void hit() {
+    }
+
   private:
     int x;
     int y;
     int strength;
+    
+    // draws the Invader
+    void draw_with_rgb(Color body_color, Color eye_color) {
+    }
 };
 
 class Cannonball {
@@ -69,10 +119,46 @@ class Cannonball {
     Cannonball() {
       x = 0;
       y = 0;
+      fired = false;
     }
+    
+    // resets private data members to initial values
+    void reset() {
+    }
+    
+    // getters
+    int get_x() const {
+    }
+    int get_y() const {
+    }
+    bool has_been_fired() const {
+    }
+    
+    // sets private data members
+    void fire(int x_arg, int y_arg) {
+    }
+    
+    // moves the Cannonball and detects if it goes off the screen
+    // Modifies: y, fired
+    void move() {
+    }
+    
+    // resets private data members to initial values
+    void hit() {
+    }
+    
+    // draws the Cannonball, if it is fired
+    void draw() {
+    }
+    
+    // draws black where the Cannonball used to be
+    void erase() {
+    }
+
   private:
     int x;
     int y;
+    bool fired;
 };
 
 class Player {
@@ -82,35 +168,87 @@ class Player {
       y = 0;
       lives = 3;
     }
+    
+    // getters
+    int get_x() const {
+    }
+    int get_y() const {
+    }
+    int get_lives() const {
+    }
+    
+    // setter
+    void set_x(int x_arg) {
+    }
+    
+    // Modifies: lives
+    void die() {
+    }
+    
+    // draws the Player
+    // calls: draw_with_rgb
+    void draw() {
+    }
+    
+    // draws black where the Player used to be
+    // calls: draw_with_rgb
+    void erase() {
+    }
+
   private:
     int x;
     int y;
     int lives;
+
+    // sets private data members x and y to initial values
+    void initialize(int x_arg, int y_arg) {
+    }
+    
+    // draws the player
+    void draw_with_rgb(Color color) {
+    }
 };
 
 class Game {
   public:
     Game() {
       level = 0;
+      time = 0;
     }
-
-    // Requires: Nothing.
+    
+    // returns enemy at specified index, if in bounds
+    Invader get_enemy(int index) {
+    }
+    
+    // sets up a new game of Space Invaders
     // Modifies: global variable matrix
-    // Effects: sets up a new game of Space Invaders
+    // see http://arduino.cc/en/Reference/Setup
     void setup() {
+      Serial.begin(9600);
+      pinMode(BUTTON_PIN_NUMBER, INPUT);
+      matrix.begin();
     }
-
-    // Requires: Nothing.
+    
+    // displays and runs Space Invaders!
+    // see spec for details of game
     // Modifies: global variable matrix
-    // Effects: displays a runs Space Invaders!
     void loop(int potentiometer_value, bool button_pressed) {
     }
 
   private:
     int level;
+    int time;
     Player player;
     Cannonball ball;
     Invader enemies[NUM_ENEMIES];
+
+    // check if Player defeated all Invaders in current level
+    bool level_cleared() {
+    }
+
+    // if Player dies, need to play the current level again.
+    void reset_level() {
+    }
 };
 
 // a global variable that represents the game Space Invaders
@@ -118,17 +256,28 @@ Game game;
 
 // see http://arduino.cc/en/Reference/Setup
 void setup() {
-  Serial.begin(9600);
-  pinMode(BUTTON_PIN_NUMBER, INPUT);
-  matrix.begin();
-
-  game.setup();
 }
 
 // see http://arduino.cc/en/Reference/Loop
 void loop() {
   int potentiometer_value = analogRead(POTENTIOMETER_PIN_NUMBER);
-  bool button_pressed = (digitalRead(BUTTON_PIN_NUMBER) == HIGH);
-
+  bool button_pressed = (digitalRead(BUTTON_PIN_NUMBER));
+  
   game.loop(potentiometer_value, button_pressed);
+}
+
+// displays Level
+void print_level(int level) {
+}
+
+// displays number of lives
+void print_lives(int lives) {
+}
+
+// displays "game over"
+void game_over() {
+}
+
+// displays "you win!"
+void player_wins() {
 }
